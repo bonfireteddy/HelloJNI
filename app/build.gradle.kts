acac4d0760ef612d8858c9abcacf1a6ee02d67f9
yountaeseop <jangmj6931@gmail.com>
2024-07-16 14:18:16 +0900

    HUCORE버전에 맞게 변경 완성

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,34 +5,21 @@ plugins {
 
 android {
     namespace = "com.example.hellojni"
-    compileSdk = 34
+    compileSdk = 33
     ndkVersion = "23.0.7599858"
 
     defaultConfig {
         applicationId = "com.example.hellojni"
-        minSdk = 33
-        targetSdk = 34
+        minSdk = 24
+        targetSdk = 33
         versionCode = 1
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
-//        ndk {
-//            abiFilters += "armeabi-v7a"
-//            abiFilters += "arm64-v8a"
-//            abiFilters += "x86"
-//            abiFilters += "x86_64"
-//        }
-
         externalNativeBuild {
             cmake {
                 cppFlags("")
-            }
-        }
-
-        sourceSets {
-            getByName("main") {
-                jniLibs.srcDir("src/main/cpp/libs")
             }
         }
     }
@@ -56,30 +43,26 @@ android {
             path ("src/main/cpp/CMakeLists.txt")
         }
 
-        viewBinding {
-            enable = true
+//        viewBinding {
+//            enable = true
+//        }
+        buildFeatures {
+            viewBinding = true
         }
     }
-
+    
     kotlinOptions {
         jvmTarget = "1.8"
-    }
-
-    packagingOptions {
-        pickFirst("lib/armeabi-v7a/libnative-lib.so")
-        pickFirst("lib/x86_64/libnative-lib.so")
-        pickFirst("lib/x86_64/libnative-lib.so")
     }
 
 }
 
 dependencies {
-
-    implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.2.0")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.0")
+    implementation("androidx.core:core-ktx:1.6.0")
+    implementation("androidx.appcompat:appcompat:1.3.1")
+    implementation("com.google.android.material:material:1.4.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.1")
+    testImplementation("junit:junit:4.+")
+    androidTestImplementation("androidx.test.ext:junit:1.1.3")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
 }
