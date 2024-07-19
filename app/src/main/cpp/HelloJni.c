590e249c3eee643a13c7d12e93b1a0cb93c21107
yountaeseop <jangmj6931@gmail.com>
2024-07-19 15:57:56 +0900

    HelloJni.c delete

--- app/src/main/cpp/HelloJni.c
+++
@@ -1,7 +0,0 @@
-#include <jni.h>
-
-JNIEXPORT jstring JNICALL
-Java_com_example_hellojni_MainActivity_getString(JNIEnv *env, jobject instance) {
-    return (*env)->  NewStringUTF(env, "Hello Jni!");
-}
-
